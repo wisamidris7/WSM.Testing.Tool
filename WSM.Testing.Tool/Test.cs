68d01dc6bf9f5ad7b2eb71ad3ff3d149e99bd837
Wisam idris <wisamidris7@gmail.com>
2023-06-24 13:37:04 -0700

    Making Static

--- WSM.Testing.Tool/Test.cs
+++ WSM.Testing.Tool/Test.cs
@@ -1,46 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace WSM.Testing.Tool
 {
-    public class Test
+    public static class Test
     {
-        public string? GetValue(string value, string end)
+        public static string? GetValue(string value, string end)
         {
             Console.Write($"{value} {end} ");
             return Console.ReadLine();
         }
-        public string? GetValue(string value)
+        public static string? GetValue(string value)
         {
             Console.Write($"{value} : ");
             return Console.ReadLine();
         }
-        public void Write(string value)
+        public static void Write(string value)
         {
             Console.Write(value);
         }
-        public void WriteLine(string value)
+        public static void WriteLine(string value)
         {
             Console.WriteLine(value);
         }
-        public void Write(params object[] values)
+        public static void Write(params object[] values)
         {
             Console.Write(values);
         }
-        public void WriteLine(params object[] values)
+        public static void WriteLine(params object[] values)
         {
             Console.WriteLine(values);
         }
-        public void Write(object value)
+        public static void Write(object value)
         {
             Console.Write($"{value}");
         }
-        public void WriteLine(object value)
+        public static void WriteLine(object value)
         {
             Console.WriteLine($"{value}");
         }
-        public void Run(List<TestItem> values)
+        public static void Run(List<TestItem> values)
         {
             Console.WriteLine("Write Any Of This Options");
             Console.WriteLine();
@@ -64,37 +63,5 @@ namespace WSM.Testing.Tool
                 }
             }
         }
-    }
-    public class TestItem
-    {
-        public TestItem()
-        {
-        }
-        public TestItem(string name, string desc, Action action)
-        {
-            Name = name;
-            Desc = desc;
-            Action = action;
-        }
-        public TestItem(string name, Action action)
-        {
-            Name = name;
-            Action = action;
-        }
-        public TestItem(string name, string desc, Func<Task> action)
-        {
-            Name = name;
-            Desc = desc;
-            Action = () => action();
-        }
-        public TestItem(string name, Func<Task> action)
-        {
-            Name = name;
-            Action = () => action();
-        }
-
-        public string Name { get; set; }
-        public string Desc { get; set; }
-        public Action Action { get; set; }
     }
 }
---
+++ WSM.Testing.Tool/TestItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WSM.Testing.Tool
+{
+    public class TestItem
+    {
+        public TestItem()
+        {
+        }
+        public TestItem(string name, string desc, Action action)
+        {
+            Name = name;
+            Desc = desc;
+            Action = action;
+        }
+        public TestItem(string name, Action action)
+        {
+            Name = name;
+            Action = action;
+        }
+        public TestItem(string name, string desc, Func<Task> action)
+        {
+            Name = name;
+            Desc = desc;
+            Action = () => action();
+        }
+        public TestItem(string name, Func<Task> action)
+        {
+            Name = name;
+            Action = () => action();
+        }
+
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public Action Action { get; set; }
+    }
+}
