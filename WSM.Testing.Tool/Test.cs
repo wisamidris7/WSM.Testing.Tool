ce14fb9a0205865e18ac1ba49a06727bd9195565
Wisam idris <wisamidris7@gmail.com>
2023-06-24 12:45:11 -0700

    Add project files.

---
+++ WSM.Testing.Tool/Test.cs
@@ -0,0 +1,52 @@
+﻿namespace WSM.Testing.Tool
+{
+    public class Test
+    {
+        public string? GetValue(string value)
+        {
+            Console.Write($"{value} : ");
+            return Console.ReadLine();
+        }
+        public void Write(string value)
+        {
+            Console.Write(value);
+        }
+        public void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Write(object value)
+        {
+            Console.Write($"{value}");
+        }
+        public void WriteLine(object value)
+        {
+            Console.WriteLine($"{value}");
+        }
+        public void Run(List<(string name, string desc, Action action)> values)
+        {
+            Console.WriteLine("Write Any Of This Options");
+            Console.WriteLine();
+            foreach (var item in values)
+            {
+                Console.WriteLine($"{item.name} For => {item.desc}");
+            }
+            Console.Write("What Option : ");
+            var line = Console.ReadLine();
+            foreach (var item in values)
+            {
+                if (line == item.name)
+                {
+                    item.action();
+                    break;
+                }
+                if (line == item.desc)
+                {
+                    item.action();
+                    break;
+                }
+            }
+        }
+    }
+}
